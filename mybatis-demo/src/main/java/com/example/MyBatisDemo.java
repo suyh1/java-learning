c30b5bade63c9bf1fbf996308cd6551b1177c7c9
suyh1 <suyhem@163.com>
2023-12-01 01:01:09 +0800

    add now

---
+++ mybatis-demo/src/main/java/com/example/MyBatisDemo.java
@@ -0,0 +1,19 @@
+package com.example;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisDemo {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+    }
+}
